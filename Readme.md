61cc07375f80d883c8951e264bac67b481245d3f
Egor Nazarov <enazarov@gmail.com>
2022-08-23 20:32:44 +0200

    first version of build scripts

---
+++ Readme.md
@@ -0,0 +1,10 @@
+ Build scripts for OpenMW for M1 Mac
+  
+ Assumes there are 2 directiries inside this one:
+ 1. openmw - https://gitlab.com/OpenMW/openmw
+ 2. openmw-deps-mac - https://gitlab.com/OpenMW/openmw-deps
+
+ result of deps build must be in ./openmw-deps-mac/build/openmw-deps/
+
+ build.sh generates .app file for OpenMW and OpenMW-CS
+ release.sh generates .dmg files 
---
+++ build.sh
@@ -0,0 +1,7 @@
+# Adjust these paths according to your setup
+export PATH_TO_DEPENDENCIES_DIR=/Users/nazarovegor/code/personal/openmw/openmw-deps-mac/build/openmw-deps
+export PATH_TO_QT_INSTALLATION=/opt/homebrew/Cellar/qt@5/5.15.5_1/lib/cmake
+
+cd openmw
+cmake -D CMAKE_PREFIX_PATH="$PATH_TO_DEPENDENCIES_DIR;$PATH_TO_QT_INSTALLATION" -D CMAKE_BUILD_TYPE=Debug -G"Unix Makefiles" ../openmw/
+make
